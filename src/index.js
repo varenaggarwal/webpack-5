dccdbb6ea10f9eaa332afc6db37b82cd7d60b075
Varen Aggarwal <varenaggarwal@gmail.com>
2021-05-17 15:37:15 +0530

    ex11: lazy loading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-import App from "./App";
-// import Logo
 import Logo from "./logo.svg";
 
-// inside innerHTML
 const root = document.getElementById("root");
-root.innerHTML = `
-<img src="${Logo}" alt="build icon" />;
-<h1>Learning Webpack tooling</h1>
-${App({ name: "Varen" })}
+const name = "Tanay";
+let route;
+
+function init() {
+  root.innerHTML = `
+        <img src="${Logo}" alt="build icon" />
+        <h1> vanillaJS app Builder </h1>
+        <button id="about"> About </button>
+        <button id="programs"> Programs </button> <!-- homework -->
+        <div id="route"></div>
 `;
+
+  route = document.querySelector("#route");
+
+  document.querySelector("#about").addEventListener("click", showAboutPage);
+}
+
+function showLoader(root) {
+  const loader = document.createElement("div");
+  loader.innerText = "loading..";
+  root.appendChild(loader);
+  return loader;
+}
+
+function hideLoader(root, loader) {
+  root.removeChild(loader);
+}
+
+function showAboutPage(event) {
+  const loader = showLoader(root);
+
+  // THIS IS THE MAIN CODE FOR LAZY LOADING
+  import(/* webpackPrefetch: true */ "./App").then(({ default: App }) => {
+    hideLoader(root, loader);
+    route.innerHTML = App({ name });
+  });
+}
+
+init();
